9918db2cbe4384734222a121deb7dcb0f6b0b22d
chunhongZheng <824394795@qq.com>
2019-08-23 00:14:21 +0800

    react next 测试

---
+++ pages/Button.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+  render() {
+
+      return <button>按钮页面</button>;
+  }
+}
+
+export default Button; // Don’t forget to use export default!
