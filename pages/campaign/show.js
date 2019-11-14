baceb317e42fed674b380395abdea3661e555cdc
chunhongZheng <824394795@qq.com>
2019-11-14 23:58:54 +0800

    创建动态众筹实例

--- pages/campaign/show.js
+++ pages/campaign/show.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import Layout from '../../component/Layout'
-
+import Campaign from '../../ethereum/campaign';
 
 class CompaignShow extends React.Component{
 
   static async getInitialProps(props){
-    console.log(props.query.address);
-    return {};
+  //  console.log(props.query.address);
+   const campaign= Campaign(props.query.address);  //构建众筹实例
+   const summary=await campaign.methods.getSummary().call();
+   //console.log(summary);
+   return {
+     contractAddress: props.query.address,
+     minunContribute: summary[0],
+     balance: summary[1],
+     requestCount:summary[2],
+     approvesCount:summary[3],
+     manager:summary[4]
+   };  //存储在props对象里面
   }
    render(){
 
+    const{
+    contractAddress,
+    minunContribute,
+    balance,
+    requestCount,
+    approvesCount,
+    manager
+    }=this.props;
+
+    console.log("contractAddress=="+contractAddress);
+    console.log("minunContribute=="+minunContribute);
+    console.log("manager=="+manager);
+
       return (
         <Layout>
-          <h4>{this.props.url.query.address}</h4>
           <h3>hello caspar</h3>
         </Layout>
       )
