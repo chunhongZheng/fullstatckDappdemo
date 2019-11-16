9bbdb706985cc9b7b6b64e8c2bc54b70ad5903e9
chunhongZheng <824394795@qq.com>
2019-11-17 06:17:34 +0800

    疯狂的js代码Array(requestCount).fill().map((element,index)=>{    return campaign.methods.requests(index).call(); })

--- pages/campaign/request/index.js
+++ pages/campaign/request/index.js
@@ -2,14 +2,23 @@ import React from 'react';
 import {Link} from '../../../routes';
 import {Button} from 'semantic-ui-react';
 import Layout from '../../../component/Layout';
+import Campaign from '../../../ethereum/campaign';
 class CompaignRequestIndex extends React.Component{
 
     static async getInitialProps(props){
        const {address}=props.query;
-       return {address};
+       const campaign= Campaign(address);
+       const requestCount=await campaign.methods.getRequestCount().call();
+       //获取所有的requests,并将之填充至数组变量requests
+       const requests = await Promise.all(
+           Array(requestCount).fill().map((element,index)=>{
+              return campaign.methods.requests(index).call();
+           })
+       );
+       return {address,requestCount,requests};
     }
     render(){
-  //  console.log("请求页面::合约地址"+this.props.address);
+    console.log("请求列表数据"+this.props.requests);
     return (
        <Layout>
             <h3>请求列表</h3>
